f329726c665d506bc07d9674ed8af6320d923c10
Abdelrahman-Mahfouz <mahfouzz427@gmail.com>
2025-01-26 20:22:21 +0200

    Merge branch 'main' of https://github.com/Mohamed-S-Helal/odoo-course-2025

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+
+print("ibrahim abdallah")
+print('hello')
+
 print('hussein tarek')
 print('Ibrahim')
 
 #####
 
 print("Abdelrahman")
+
+print('omer')
+print('halla madrid')
+print("Mhmd shawkii")
