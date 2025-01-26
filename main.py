353d7c1bb3700986abf7c3de968fc6796e2827ba
Ahmeddaoud1996 <ahmed.omer.daoud1996@gmail.com>
2025-01-26 21:56:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 
-print("ibrahim abdallah")
-print('hello')
 
-print('hussein tarek')
-print('omer')
-print('halla madrid')
-print("Mhmd shawkii  ")
-print('halla madrid 2')
+
+class Employee:
+    pass
