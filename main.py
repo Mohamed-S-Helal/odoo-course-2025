fcb94deca115a316b3f44a5eaa363808e129b0cc
ibrahim abdallah <89431527+IbrahimGhorab@users.noreply.github.com>
2025-01-26 20:25:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 
 print("ibrahim abdallah")
 print('hello')
-
 print('hussein tarek')
 print('Ibrahim')
-
-#####
-
 print("Abdelrahman")
-
 print('omer')
 print('halla madrid')
 print("Mhmd shawkii")
