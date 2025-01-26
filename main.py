8596108e3d7b0bd284965b61a5473d95bf454e39
husseintarek12 <husseintarek12@gmail.com>
2025-01-26 21:53:13 +0200

    Last Update (Hussein_Tarek)

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hussein tarek')
+#print('hussein tarek')
+
+#OPP
+
+#new
+class Employee:
+    def __init__(self, emp_name, emp_salary):
+        print('hello')
+        self.name = emp_name
+        self.__salary = emp_salary
+
+emp1 = Employee('ahmed', 5000)
+emp2 = Employee('hussein', 7000)
+print(emp1.name)
+print(emp2.name)
