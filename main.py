cc7ba782757b24aadf8708b3ac2631ecd7892f41
husseintarek12 <husseintarek12@gmail.com>
2025-01-28 02:09:04 +0200

    Update Tax And Net. Salary For Employee
    calculate the tax
    calculate the salary after the tax
    = Net. Salary

--- main.py
+++ main.py
@@ -12,22 +12,43 @@ class Employee:
         self.__salary = emp_salary
         self.location = emp_location
 
+    def get_salary(self):
+            return self.__salary
+
+    def tax_salary(self):
+        tax_amount = self.__salary * 0.10   # calculate the tax = 10% TAX
+        return tax_amount
+
+    def net_salary(self):
+        net_salary = self.__salary - self.tax_salary()  # calculate the salary after the tax
+        return net_salary
+
     def display_info(self):
         print(f"Employee ID: {self.id}")
         print(f"Name: {self.name}")
         print(f"Department: {self.department}")
         print(f"Jop Title: {self.jop_title}")
-        print(f"Salary: {self.__salary} LE")
+        #TAX And Permission
+        if self.get_salary() >= 10000:
+            print(f"Salary: None")
+            print(f"Tax: None")
+            print(f"Salary: None")
+        else:
+            print(f"Salary: {self.get_salary()} LE")
+            print(f"Tax: {self.tax_salary()} LE")
+            print(f"Net.Salary: {self.net_salary()} LE")
         print(f"Location: {self.location}\n")
 
 
+
+
 emp1 = Employee(1, 'Hussein',
-                'Accounts', 'Operation Dev',
-                6500, 'Administration')
+                'Operation', 'Operation Dev',
+                12000, 'Administration')
 
 emp2 = Employee(2, 'Mahmoud',
                 'Inventory' , 'Store Keeper',
-                4500, 'Branch')
+                5500, 'Branch')
 
 emp3 = Employee(3, 'Ahmed',
                 'Accounts', 'Accountant',
@@ -35,15 +56,15 @@ emp3 = Employee(3, 'Ahmed',
 
 emp4 = Employee(4, 'Tamer',
                 'Sales', 'Sales Representative',
-                4500 ,'Branch')
+                6000 ,'Branch')
 
 emp5 = Employee(5, 'Mohamed',
                 'Purchases', 'Purchasing Representative',
-                5500 ,'Branch')
+                6000 ,'Branch')
 
 emp6 = Employee(6, 'Momen',
                 'HR', 'Buffet Worker',
-                3000 ,'Branch')
+                4000 ,'Branch')
 
 emp1.display_info()
 emp2.display_info()
