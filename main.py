721bc01693d095e5e2c338d51558c80445095c71
husseintarek12 <husseintarek12@gmail.com>
2025-01-28 04:57:46 +0200

    Update With All Att & Func For Session 6

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#print('hussein tarek')
-
 #OPP
 
-#new
+# Polymorphism Format
 class Employee:
     def __init__(self, emp_id, emp_name, emp_department, emp_jop_title, emp_salary, emp_location):
+        # __init__ Holds A Set Of Attribute Like (id, name, salary, ,)
         self.id = emp_id
         self.name = emp_name
         self.department = emp_department
@@ -12,36 +11,41 @@ class Employee:
         self.__salary = emp_salary
         self.location = emp_location
 
+    # And We Can Create Functions That Serve The Attribute
+
+    def set_salary(self, new_salary):
+        self.__salary = new_salary # Function To Modify Salary
+
     def get_salary(self):
-            return self.__salary
+        return self.__salary # To Use On if Condition Line 34 & Print Line 40
 
     def tax_salary(self):
-        tax_amount = self.__salary * 0.10   # calculate the tax = 10% TAX
+        tax_amount = self.__salary * 0.10   # Calculate The Tax = 10% TAX
         return tax_amount
 
     def net_salary(self):
-        net_salary = self.__salary - self.tax_salary()  # calculate the salary after the tax
+        net_salary = self.__salary - self.tax_salary()  # Calculate The Salary After The TAX
         return net_salary
 
     def display_info(self):
         print(f"Employee ID: {self.id}")
         print(f"Name: {self.name}")
         print(f"Department: {self.department}")
-        print(f"Jop Title: {self.jop_title}")
-        #TAX And Permission
+        print(f"Job Title: {self.jop_title}")
+        # Permission
         if self.get_salary() >= 10000:
             print(f"Salary: None")
             print(f"Tax: None")
             print(f"Salary: None")
+        # Calculate TAX
         else:
-            print(f"Salary: {self.get_salary()} LE")
-            print(f"Tax: {self.tax_salary()} LE")
-            print(f"Net.Salary: {self.net_salary()} LE")
+            print(f"Salary: {self.get_salary()} LE")      # salary before tax
+            print(f"Tax: {self.tax_salary()} LE")         # salary tax value
+            print(f"Net.Salary: {self.net_salary()} LE")  # salary after tax
         print(f"Location: {self.location}\n")
 
 
-
-
+# Employees Info
 emp1 = Employee(1, 'Hussein',
                 'Operation', 'Operation Dev',
                 12000, 'Administration')
@@ -66,9 +70,56 @@ emp6 = Employee(6, 'Momen',
                 'HR', 'Buffet Worker',
                 4000 ,'Branch')
 
+# Polymorphism Format (Child)
+class Manager(Employee):
+    def __init__(self, manag_id, emp_id, emp_name, emp_department,
+                 emp_jop_title, emp_salary, emp_location, emp_chart):
+        super().__init__(emp_id, emp_name, emp_department,
+                         emp_jop_title, emp_salary, emp_location)
+        self.organization_chart = emp_chart
+        self.manag_id = manag_id
+
+    def display_info(self):
+        print(f"Manag ID: {self.manag_id}")
+        super().display_info()
+        print(f"Organization Chart: {self.organization_chart}\n")
+
+
+# Manger Info
+manag1 = Manager(1, 7, 'Mostafa',
+                'Operation', 'Operation Manager',
+                20000, 'Administration', f"\nID: {emp1.id} - Name: {emp1.name}")
+
+manag2 = Manager(2, 8, 'Yasser',
+                'Finances', 'Financial Manager',
+                18000, 'Administration', f"\nID: {emp3.id} - Name: {emp3.name}\n"
+                                         f"ID: {emp4.id} - Name: {emp4.name}")
+
+manag3 = Manager(3, 9, 'Essam',
+                'Inventory', 'Inventory Manger',
+                15000, 'Administration', f"\nID: {emp2.id} - Name: {emp2.name}\n"
+                                         f"ID: {emp5.id} - Name: {emp5.name}\n"
+                                         f"ID: {emp6.id} - Name: {emp6.name}")
+
+
+# U Can Add New Salary Here, But It Must Be Before (.Display_info)
+# Or Make New (.display_info) One For First Version And One For Last Updated Version Under The (set_salary)
+
+
+# First Ver Of Employees
+emp4.display_info()
+
+# Set New Salary For Employees
+emp4.set_salary(6500)
+
+# Last Updated Ver Of Employee
 emp1.display_info()
 emp2.display_info()
 emp3.display_info()
 emp4.display_info()
 emp5.display_info()
 emp6.display_info()
+
+manag1.display_info()
+manag2.display_info()
+manag3.display_info()
